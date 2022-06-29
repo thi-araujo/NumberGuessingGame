fa4bbf594387298b93d79658bc151716d46276e9
Thiago Araújo <97451338+thi-araujo@users.noreply.github.com>
2022-06-29 14:50:06 -0300

    "função show_score"

--- JogoAdivinhacaoNumeros.py
+++ JogoAdivinhacaoNumeros.py
@@ -1,7 +1,10 @@
 #---------------------------------------JOGO ADIVINHAÇÃO DE NÚMEROS----------------------------------------------------
 
 import random
-attempts_list = []
+lista_de_tentativas = []
 def show_score():
     if len(attempts_list) <= 0:
         print("Ate o momento atual não existe pontuação alta, é sua vez para o ponta pé inicial ")
+
+    else:
+        print("A pontuação mais alta é de {} tentativas".format(min(lista_de_tentativas)))
