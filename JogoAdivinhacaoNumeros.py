baf60c0b06222b51600abf56554b77e2453b3ea1
Thiago Araújo <97451338+thi-araujo@users.noreply.github.com>
2022-07-01 14:29:30 -0300

    "Função show_score"

--- JogoAdivinhacaoNumeros.py
+++ JogoAdivinhacaoNumeros.py
@@ -1,7 +1,42 @@
 #---------------------------------------JOGO ADIVINHAÇÃO DE NÚMEROS----------------------------------------------------
 
 import random
-attempts_list = []
+lista_tentativa = []
+
+# funcão
 def show_score():
-    if len(attempts_list) <= 0:
+    if len(lista_tentativa) <= 0:
         print("Ate o momento atual não existe pontuação alta, é sua vez para o ponta pé inicial ")
+
+
+    else:
+        print("A pontuação mais alta atual é de {} tentativas".format(min(lista_tentativa)))
+
+def start_game():
+    random_number = int(random.randint(1, 10))
+
+    print("Olá Jogador! Bem Vindo ao Game de Adivinhação!")
+
+    player_name = input("Digite seu nome: ")
+
+    wanna_play = input("Olá, {}, voce gostaria de jogar o Game de Adivinhação? (Digite: Yes/No) ".format(player_name))
+
+    # Função show_score
+
+    tentativa = 0
+
+    show_score()
+    while wanna_play.lower() == "yes":
+        try:
+
+        guess = input("Escolha um número entre 1 e 10 ")
+
+        if int(guess) < 1 or int(guess) > 10:
+
+        raise ValueError("Por favor, adivinhe um número dentro do intervalo fornecido")
+
+        if int(guess) == random_number:
+
+        print("Muito bom! Você entendeu!")
+
+
