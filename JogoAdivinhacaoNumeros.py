674dd286fe254a35efe63865b1b5776577f76b0b
Thiago Araújo <97451338+thi-araujo@users.noreply.github.com>
2022-07-10 22:04:54 -0300

    #erro de  identação"

--- JogoAdivinhacaoNumeros.py
+++ JogoAdivinhacaoNumeros.py
@@ -5,26 +5,16 @@ import random
 lista_tentativa = []
 
 def show_score():
-
-	 if len(lista_tentativa) <= 0:
-
-		print("Ate o momento atual não existe pontuação alta, é sua vez para o ponta pé inicial ")
-
+    if len(lista_tentativa) <= 0:
+        print("Ate o momento atual não existe pontuação alta, é sua vez para o ponta pé inicial ")
 	else:
-
 		print("A pontuação mais alta atual é de {} tentativas".format(min(lista_tentativa)))
 
 def start_game():
-
-
 	random_number = int(random.randint(1, 10))
-
-
 	print("Olá Jogador! Bem Vindo ao Game de Adivinhação!")
 
-
 player_name = input("Digite seu nome: ")
-
 
 wanna_play = input("Olá, {}, voce gostaria de jogar o Game de Adivinhação? (Digite: Sim/Não) ".format(player_name))
 
@@ -35,19 +25,18 @@ tentativa = 0
 show_score()
 
 while wanna_play.lower() == "sim":
-
     try:
         guess = input("Escolha um número entre 1 e 10")
-
     if int(guess) < 1 or int(guess) > 10:
         raise ValueError("Por favor, adivinhe um número dentro do intervalo fornecido")
-
     if int(guess) == random_number:
         print("Muito bom! Você entendeu!")
 
-        tentativa += 1
-        lista_tentativa.append(tentativa)
-        print("Foram necessárias {} tentativas".format(tentativa))
+tentativa += 1
+
+lista_tentativa.append(tentativa)
+print("Foram necessárias {} tentativas".format(tentativa))
+tentativa = 0
 
     jogar_de_novo = input("Gostaria de jogar novamente? Digite Sim/Não")
     tentativa = 0
